9a6d9a51e44158dce8334ca5ec1aeb51219c4faf
Rajat <rajat804@gmail.com>
2026-06-30 10:37:07 +0530

    gr sequence add

--- src/models/GrSequence.js
+++ src/models/GrSequence.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const grSequenceSchema = new mongoose.Schema({
   branch: { type: String, required: true, unique: true },
-  prefix: { type: String, required: true },    // e.g., "KH", "CC"
-  currentNumber: { type: Number, default: 1 }, // 1 to 50
+  prefix: { type: String, required: true },
+  currentNumber: { type: Number, default: 1 },
   maxLimit: { type: Number, default: 50 },
 }, { timestamps: true });
 
